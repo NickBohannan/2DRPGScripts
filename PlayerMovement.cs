487683674c2d0af7f57a9c233e3ce3cdc2ec0d33
Nick Bohannan <nbohannan@gmail.com>
2020-11-04 22:30:53 -0600

    first push of scripts to GitHub

---
+++ PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Vector2 lastPosition;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public Sprite startingSprite;
+
+    Vector2 movement;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animator.enabled);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (rb.position.x == lastPosition.x && rb.position.y == lastPosition.y)
+        {
+            animator.enabled = false;
+            spriteRenderer.sprite = startingSprite;
+        }
+        else
+        {
+            animator.enabled = true;
+        }
+
+        lastPosition.x = rb.position.x;
+        lastPosition.y = rb.position.y;
+    }
+
+    /*private void toggleWalkAnimation()
+    {
+        if ()
+    }
+    */
+}
