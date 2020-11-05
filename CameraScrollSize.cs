4bb56e0c55ee966b050226db434bc337ce078746
Nick Bohannan <nbohannan@gmail.com>
2020-11-04 23:26:13 -0600

    add camera scroll sizing

--- CameraScrollSize.cs
+++ CameraScrollSize.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraScrollSize : MonoBehaviour
 {
+    public int sizeMin, sizeMax;
+    public Camera topCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        topCamera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.mouseScrollDelta.y > 0 && topCamera.orthographicSize > sizeMin)
+        {
+            topCamera.orthographicSize -= 1;
+        } else if (Input.mouseScrollDelta.y < 0 && topCamera.orthographicSize < sizeMax)
+        {
+            topCamera.orthographicSize += 1;
+        } 
     }
 }
